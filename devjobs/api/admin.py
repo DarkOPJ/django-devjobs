fd119fe8e8398ea542fd49e9c0e56ae93e58c6a8
Boadu Aaron Jeffrey Bosomepem <100956199+DarkOPJ@users.noreply.github.com>
2026-05-28 21:55:23 +0000

    feat: initialize API models, add REST framework configuration, and scaffold user_auth app

--- devjobs/api/admin.py
+++ devjobs/api/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Job, Company
 
 # Register your models here.
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'contact_email', 'contact_phone')
+
+@admin.register(Job)
+class JobsAdmin(admin.ModelAdmin):
+    list_display = ('title', 'company', 'location', 'salary')
--- devjobs/api/models.py
+++ devjobs/api/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+
+class Company(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=80)
+    description = models.TextField(max_length=400)
+    contact_email = models.EmailField()
+    contact_phone = models.CharField(max_length=20)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+class Job(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    company = models.ForeignKey(
+        Company,
+        on_delete=models.CASCADE,   # if company is deleted, its jobs are deleted too
+        related_name='jobs'         # lets you do company.jobs.all() later
+    )
+    title = models.CharField(max_length=80)
+    job_type = models.CharField(max_length=80)
+    description = models.TextField(max_length=400)
+    location = models.CharField(max_length=80)
+    salary = models.CharField(max_length=20)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.title} at {self.company.name}"
+    
+    
