3854521852408aa9fbed9329b9ee7d7dd5997b48
Boadu Aaron Jeffrey Bosomepem <100956199+DarkOPJ@users.noreply.github.com>
2026-05-29 04:05:22 +0000

    chore: refactor settings to use environment variables for security and configuration

--- devjobs/devjobs/settings.py
+++ devjobs/devjobs/settings.py
@@ -25,16 +25,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_rxeeg!0&&869qudg2fgl(moqcu8#6dl^h24ucn$7+3-^1d7iw'
+# SECRET_KEY = 'django-insecure-_rxeeg!0&&869qudg2fgl(moqcu8#6dl^h24ucn$7+3-^1d7iw'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+# ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
 
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:4000',
-]
+# CORS_ALLOWED_ORIGINS = [
+#     'http://localhost:4000',
+# ]
+CORS_ALLOWED_ORIGINS = os.environ.get("CORS_ALLOWED_ORIGINS","").split(",")
 
 # Application definition
 
